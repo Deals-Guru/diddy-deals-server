54d470eac5e2fa4a64e4b22aa5b1628d94904493
Tushar656 <ui20cs69@iiitsurat.ac.in>
2025-05-31 23:23:10 +0530

    netlify cors

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,22 @@
 const serverless = require('serverless-http');
-const app = require('../../index.js');
+const app = require('../../index');
 
-exports.handler = serverless(app);
+exports.handler = serverless(app, {
+  binary: ['image/*', 'application/*'],
+  response: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true',
+    },
+    onError: (err, event, context) => {
+      return {
+        statusCode: err.statusCode || 500,
+        body: JSON.stringify({ error: err.message }),
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': 'true',
+        },
+      };
+    }
+  }
+});

308062da54e4b47e553e6e1351c455cd6fbb815c
Tushar656 <ui20cs69@iiitsurat.ac.in>
2025-05-31 22:04:29 +0530

    netlify deploy change file path

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const adminRoutes = require('../../routes/adminRoutes');
-const productRoutes = require('../../routes/productRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 const port = process.env.PORT || 5000;
